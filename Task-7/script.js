2ba5ab4ae5d4ac0b5be627c898e5b1fec34ab029
SiarheiHarkusha <75556025+SiarheiHarkusha@users.noreply.github.com>
2021-04-05 17:46:56 +0300

    Add files via upload

---
+++ Task-7/script.js
@@ -0,0 +1,65 @@
+// 1.Счетчик - сделать кнопку, которая будет увеличивать значение счетчика на +1.
+
+// const minus = document.createElement('button');
+// const left = document.createTextNode( " - " );
+// minus.appendChild( left )
+// document.body.appendChild( minus );
+// document.body.appendChild( minus ).onclick = neg;
+
+// let div = document.createElement('input');
+// document.body.append(div);
+// div.value = '0'; 
+// div.style.textAlign = 'center';
+
+
+
+
+
+// const plus = document.createElement('button');
+// const right = document.createTextNode(" + ")
+// plus.appendChild( right )
+// document.body.appendChild( plus ).onclick = sum;
+
+
+// let cifra = 0;
+// function sum () {
+//     div.value = cifra++;
+// }
+
+// function neg () {
+//     div.value = cifra--;
+// }
+
+
+
+
+
+
+
+// 2. todo list - написать туду лист состоящий из инпута(куда пользователь будет вводить свой туду) и кнопки(по нажатию на которую туду будет добавляться в список с остальными туду); список туду сделать используя ul и li
+
+// const button = document.createElement( 'button');
+// document.body.append( button );
+// button.innerHTML = 'ADD';
+// button.id = 'knopka'
+
+// let input = document.createElement( 'input');
+// document.body.append( input );
+
+// let ol  = document.createElement( 'ol' );
+// document.body.append( ol );
+
+// function addLi() {
+//     let text = input.value;
+//     let li = document.createElement('li');
+//     if (text === '') {
+//         return
+//     }
+//     ol.append( li );
+//     li.innerHTML = text;
+//     input.value = '';
+// }
+// document.querySelector('#knopka').onclick = addLi;
+
+
+// 3. написать приложение сумматор - состоящий из 2ух инпутов куда пользователь будет вводить слагаемые и кнопку "посчитать" по нажатию на которую пользователю в диве ниже будет выведен результат вычисления
